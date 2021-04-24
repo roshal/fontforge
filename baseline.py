3b9fdeb0d5cf5c58ba1fdf120a97b82dae339a8c
roshal <roshal@users.noreply.github.com>
2021-04-25 01:31:17 +0300

    update

--- baseline.py
+++ baseline.py
@@ -6,19 +6,20 @@ import sys
 def handle(path):
 	font = fontforge.open(path)
 	print(font.hhea_ascent, font.hhea_descent, font.hhea_linegap)
+	print(font.os2_typoascent, font.os2_typodescent, font.os2_typolinegap)
+	print(font.os2_winascent, font.os2_windescent)
 	font.hhea_ascent = font.os2_winascent
 	font.hhea_descent = -font.os2_windescent
 	font.hhea_linegap = 0
-	print(font.os2_typoascent, font.os2_typodescent, font.os2_typolinegap)
 	font.os2_typoascent = font.os2_winascent
 	font.os2_typodescent = -font.os2_windescent
 	font.os2_typolinegap = 0
-	print(font.os2_winascent, font.os2_windescent)
 	return font
 
 if __name__ == '__main__':
 	arguments = sys.argv[1:]
 	for path in arguments:
-		name = os.path.basename(path)
+		basename = os.path.basename(path)
+		filename = f'baseline-{basename}'
 		font = handle(path)
-		font.save(f'baseline-{name}')
+		font.save(filename)
