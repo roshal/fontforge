6edcce3a8ed9a74f70f2b143b58393b4f3b626f2
roshal <roshal@users.noreply.github.com>
2024-06-28 21:06:50 +0300

    update

--- baseline.py
+++ baseline.py
@@ -1,11 +1,14 @@
 
-import fontforge
 import os
-import sys
+import pathlib
 
-def handle(path):
+def open_font(path: str):
 
 	font = fontforge.open(path)
+
+	return font
+
+def operate_font(font: fontforge.font):
 
 	print(font.hhea_ascent, font.hhea_descent, font.hhea_linegap)
 	print(font.os2_typoascent, font.os2_typodescent, font.os2_typolinegap)
@@ -19,14 +22,39 @@ def handle(path):
 	font.os2_typodescent = -font.os2_windescent
 	font.os2_typolinegap = 0
 
-	return font
+def get_subtitle():
+
+	filename = os.path.basename(__file__)
+
+	subtitle = os.path.splitext(filename)[0]
+
+	return subtitle
+
+def operate_arg(arg: str):
+
+	path = arg
+
+	font = open_font(path)
+
+	operate_font(font)
+
+	subtitle = get_subtitle()
+
+	filename = os.path.basename(path)
+
+	subtitlePath = pathlib.Path(subtitle)
+
+	subtitlePath.mkdir(exist_ok=True)
+
+	filePath = subtitlePath.joinpath(filename)
+
+	filepath = str(filePath)
+
+	font.generate(filepath)
 
 if __name__ == '__main__':
 
-	arguments = sys.argv[1:]
+	arg_list = sys.argv[1:]
 
-	for path in arguments:
-		basename = os.path.basename(path)
-		filename = f'baseline-{basename}'
-		font = handle(path)
-		font.save(filename)
+	for arg in arg_list:
+		operate_arg(arg)
